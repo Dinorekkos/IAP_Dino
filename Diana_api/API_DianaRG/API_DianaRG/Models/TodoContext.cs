d5f6af6db7bb90aeb254de9c667b55736cf38840
Dino <diana_ramos_g@hotmail.com>
2022-09-01 18:51:31 -0500

    SUBIR API

---
+++ Diana_api/API_DianaRG/API_DianaRG/Models/TodoContext.cs
@@ -0,0 +1,12 @@
+namespace API_DianaRG.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class TodoContext : DbContext
+{
+    public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<TodoItem> TodoItems { get; set; }
+}
---
+++ Diana_api/API_DianaRG/API_DianaRG/Models/TodoItem.cs
@@ -0,0 +1,8 @@
+namespace API_DianaRG.Models;
+
+public class TodoItem
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public bool IsComplete { get; set; }
+}
